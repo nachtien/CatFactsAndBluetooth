a0d75e4789f45b22664bf4a44e28cd2027c6340d
Nick Achtien <nachtien@users.noreply.github.com>
2024-11-20 16:50:43 -0800

    flatten dirs

--- app/src/main/kotlin/MainActivity.kt
+++ app/src/main/kotlin/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.achtien.codingtemplate
 
-import NicksViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
--- app/src/main/kotlin/NicksViewModel.kt
+++ app/src/main/kotlin/NicksViewModel.kt
@@ -1,3 +1,6 @@
+package com.achtien.codingtemplate
+
+import Repository
 import androidx.lifecycle.ViewModel
 
 class NicksViewModel(private val repository: Repository): ViewModel() {
